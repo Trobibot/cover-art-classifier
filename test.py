74bf9933103624e0193bc2e44738114ea0f0f3dd
trobibot <hugomouchel21@gmail.com>
2022-11-25 13:00:29 +0000

    Add gitignore

--- test.py
+++ test.py
@@ -8,8 +8,8 @@ import pandas as pd
 if __name__ == '__main__':
     custom_vision_project, training_client, _ = auth()
 
-    tags = pd.read_csv('./sortedTags.csv')
-    upload_tags(tags, custom_vision_project, training_client)
+    # tags = pd.read_csv('./sortedTags.csv')
+    # upload_tags(tags, custom_vision_project, training_client)
 
-    albums = pd.read_csv('./sortedAlbums.csv')
-    Upload_Images_to_Azure(albums, custom_vision_project, training_client)
+    # albums = pd.read_csv('./sortedAlbums.csv')
+    # Upload_Images_to_Azure(albums, custom_vision_project, training_client)
